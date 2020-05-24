94f85d1f3e20301b367e9cc1cebe4eab5af38cc1
Takeshi Nishio <kkeennnn@gmail.com>
2020-05-24 18:08:43 +0900

    Update コメント修正。

--- RoundRobinMatrix/assets/matrix_rr.c
+++ RoundRobinMatrix/assets/matrix_rr.c
@@ -161,7 +161,6 @@ static void init_pins(void) {
     }
 }
 
-//TODO
 static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
     // Store last value of row prior to reading
     matrix_row_t last_row_value = current_matrix[current_row];
@@ -178,13 +177,8 @@ static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
 
     // For each col...
     for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
-        // // Select the col pin to read (active low)
-        // uint8_t pin_state = readPin(col_pins[col_index]);
-        //
-        // // Populate the matrix row with the state of the col pin
-        // current_matrix[current_row] |= pin_state ? 0 : (MATRIX_ROW_SHIFTER << col_index);
 
-        // rowと同じインデックスのcol（＝同じピン）は読まない
+        // Don't read the pin it self.
         if (col_index != current_row) {
             // Check col pin pin_state
             if (readPin(col_pins[col_index]) == 0) {
